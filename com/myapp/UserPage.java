393345f423019c461c7040467a5017a70d9fc695
alinagts <godovanetsalina@gmail.com>
2022-06-20 10:21:21 +0300

    adding timer

--- com/myapp/UserPage.java
+++ com/myapp/UserPage.java
@@ -12,7 +12,7 @@ public class UserPage extends JFrame{
     final int Y_COORDINATE = 300;
     final int FRAME_WIDTH = 600;
     final int FRAME_HEIGHT = 500;
-    final int GRID_ROWS = 9;
+    final int GRID_ROWS = 10;
     final int GRID_COLUMNS = 1;
     final int VERTICAL_GAP = 10;
     final int HORIZONTAL_GAP = 2;
@@ -22,12 +22,14 @@ public class UserPage extends JFrame{
     JButton[] options = new JButton[4];
     JButton restartButton = new JButton("Start again");
     JButton backButton = new JButton("Back to login form");
+    JLabel secondsLeft = new JLabel();
     String name;
     ArrayList<String> questionArray = new ArrayList<>();
     ArrayList<String[]> optionArray = new ArrayList<>();
     ArrayList<String> correctAnswers = new ArrayList<>();
     int index = 0;
     int correctGuesses = 0;
+    int seconds = 10;
 
     public UserPage(String name) {
 
@@ -47,8 +49,10 @@ public class UserPage extends JFrame{
             options[i].addActionListener(new UserPage.ButtonEventManager());
         }
 
+        secondsLeft.setHorizontalAlignment(JTextField.CENTER);
         container.add(new JLabel("Your current score:"));
         container.add(score);
+        container.add(secondsLeft);
         container.add(restartButton);
         container.add(backButton);
 
@@ -57,6 +61,24 @@ public class UserPage extends JFrame{
 
         this.name = name;
     }
+
+    Timer timer = new Timer(1000, new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            secondsLeft.setText(String.valueOf(seconds));
+            seconds--;
+            if (seconds < 3) {
+                secondsLeft.setForeground(Color.RED);
+                if(seconds < 0) {
+                    index++;
+                    seconds = 10;
+                    nextQuestion();
+                    secondsLeft.setForeground(Color.BLACK);
+                }
+            }
+        }
+    });
 
     public void takeQuiz(ArrayList<String> questions, ArrayList<String[]> options, ArrayList<String> correctAns) {
         this.questionArray = questions;
@@ -70,16 +92,17 @@ public class UserPage extends JFrame{
 
         String currentScore = correctGuesses + "/" + questionArray.size();
 
+        score.setText(currentScore);
         if(index < questionArray.size()) {
 
             questionText.setText(questionArray.get(index));
             for(int i = 0; i < 4; i++) {
                 options[i].setText(optionArray.get(index)[i]);
             }
-
-            score.setText(currentScore);
+            timer.start();
 
         } else {
+            timer.stop();
             int percentage = Math.round(correctGuesses * 100 / questionArray.size());
             JOptionPane.showMessageDialog(null,   name + ", your score is " + currentScore + "\n" + percentage + "%");
 
@@ -97,6 +120,7 @@ public class UserPage extends JFrame{
 
             for(int i = 0; i < 4; i++) {
                 if(e.getSource() == options[i]) {
+                    seconds = 10;
                     if(options[i].getText().equals(correctAnswers.get(index))) {
                         correctGuesses++;
                     }
@@ -106,6 +130,8 @@ public class UserPage extends JFrame{
             }
 
             if(e.getSource() == restartButton) {
+                timer.stop();
+                seconds = 10;
                 for(int i = 0; i < 4; i++) {
                     options[i].setEnabled(true);
                 }
